bd5bec9f27088eb8c740423e3d21934472d4f84f
Chris Kenney <37443483+christianpug@users.noreply.github.com>
2022-07-01 09:21:31 -0400

    Version 0.0.1
    Just A test dont download just yet

---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+policy="Hello user, this is a script for installing a server for Open Fortress. Please be advised that this for that game and that game only. If you want to learn more about Open Fortress, look at their website at openfortress.fun. Do you wish to continue?"
+read $policy
+agreement=1
+non_agreement=0
+
+if $agreement 
+then
